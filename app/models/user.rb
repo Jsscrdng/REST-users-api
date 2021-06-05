04f4a66172a09ec0e5e375e84c0ff87d5d63e894
Jesus Cardenas <jesscrdng@gmail.com>
2021-06-05 10:40:40 -0500

    New User model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  ## Classification comply with ISO/IEC 5218
+  enum sex: { not_known: 0, male: 1, female: 2, not_applicable: 9 }
+end
---
+++ db/migrate/20210605152916_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :full_name
+      t.date :date_of_birth
+      t.integer :sex
+      t.text :image_url
+
+      t.timestamps
+    end
+  end
+end
