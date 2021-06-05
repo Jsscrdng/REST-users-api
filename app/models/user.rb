f2ce655ce5a91c0977623930145073953658fd44
Jesus Cardenas <jesscrdng@gmail.com>
2021-06-05 12:28:35 -0500

    Add User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   ## Classification comply with ISO/IEC 5218
   enum sex: { not_known: 0, male: 1, female: 2, not_applicable: 9 }
+
+  validates_presence_of :full_name
 end
